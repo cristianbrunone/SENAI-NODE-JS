d3fbfb355c264da0ecd268849ffb17592b5d4155
CRISTIAN MOISES BRUNONE CORDEIRO <cristian_b_cordeiro@estudante.sesisenai.org.br>
2024-05-02 21:14:20 -0300

    Parte 2 Rotas com Express

--- CristianBrunone/ListaDeGames/index.js
+++ CristianBrunone/ListaDeGames/index.js
@@ -1,10 +1,6 @@
-//carregando o express
 const express = require("express");
-//instancio o express e carregando a biblioteca do expres dentro dessa const app
 const app = express();
 
-
-//Lista de Games
 let games = [
     {title: "Sea of Thieves", studio: "Rare", price: 30},
     {title: "WOW", studio: "Blizzard", price: 120},
@@ -12,7 +8,6 @@ let games = [
     {title: "COD", studio: "Activision", price: 200},
     {title: "Minecraft", studio: "Mojang", price: 80},
     {title: "Halo", studio: "Microsoft", price: 90},
-    {title: "Fifa", studio: "EA Sports", price: 90},
     {title: "Batman", studio: "EA Sports", price: 90},
     {title: "Spiderman", studio: "Marvel", price: 80},
     {title: "Justice League", studio: "DC", price: 70},
@@ -20,9 +15,28 @@ let games = [
 
 const PORT = 3080; 
 app.listen(PORT, () => {
-    console.log(`Servidor funcionando no porto ${PORT}`);
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json(games);
+});
+
+app.post("/novogame", (req, res) => {
+    let title = req.body.title;
+    let studio = req.body.studio;
+    let price = req.body.price;
+
+    console.log(title);
+    console.log(studio);
+    console.log(price);
+
+    let newGame = { title: title, studio: studio, price: price }
+
+    // Agregamos el nuevo juego al array 'games'
+    games.push(newGame);
+
+    res.send("OK");
 });
